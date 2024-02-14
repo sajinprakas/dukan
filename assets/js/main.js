f673f5748072c9f85760258f97ecf00fd2be64f4
sajinprakas <sajinp297@gmail.com>
2024-02-14 11:43:56 +0530

    header completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,50 +53,15 @@ $(document).ready(function() {
 	})
 
 
-	function updateProgressBar(progress, progressBarId) {
-        var progressBar = document.getElementById(progressBarId).getElementsByClassName('progress-bar-inner')[0];
-        var progressText = document.getElementById(progressBarId).getElementsByClassName('progress-text')[0];
-
-        progressBar.style.width = progress + '%';
-        progressText.innerHTML = progress + '%';
-    }
-
-    function simulateProgress(totalProgress, progressBarId) {
-        var interval = 60; // milliseconds
-        var currentProgress = 0;
-
-        var progressInterval = setInterval(function () {
-            updateProgressBar(currentProgress, progressBarId);
-
-            if (currentProgress >= totalProgress) {
-                clearInterval(progressInterval);
-            } else {
-                currentProgress++;
-            }
-        }, interval);
-    }
-
-    simulateProgress(100, 'progress-bar1');
-    simulateProgress(100, 'progress-bar2');
-    simulateProgress(100, 'progress-bar3');
-	simulateProgress(100, 'progress-bar4');
-
-
-
-
-
-
-
-
 
 
 
 
 	var navigation = new TimelineLite({paused:true, reversed:true});
 	navigation.to("#navigationWrap", 0.5, {opacity: 1, display: 'block'})
-			.to(".navbar", 0.3, {opacity: 0}, "-=0.1")
-			.to(".close", 0.3, {display: "block", opacity: 1}, "-=0.1")
-			.from(".menu", 0.5, {opacity: 0, y: 30})
+			.to(".navbar", 0.1, {opacity: 0}, "-=0.1")
+			.to(".close", 0.2, {display: "block", opacity: 1}, "-=0.1")
+			.from(".menu", 0.1, {opacity: 0, y: 30})
 			.from(".social", 0.5, {opacity: 0});
 
 	$(".navbar, .close").click (function() {
